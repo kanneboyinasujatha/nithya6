2b50605283f03526990b0587fe9fd3744a2d9999
kanneboyinasujatha <41731075+kanneboyinasujatha@users.noreply.github.com>
2018-07-27 11:03:53 +0530

    Create larger.java

---
+++ larger.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		System.out.println("enter num1,num2,num3:");
+		int num1=sc.nextInt();
+		int num2=sc.nextInt();
+		int num3=sc.nextInt();
+		if((num1>num2)&&(num1>num3))
+		System.out.print(num1);
+		else if((num2>num1)&&(num2>num3))
+		System.out.print(num2);
+		else
+		System.out.print(num3);
+		
+	}
+}
